d3a818842270304de703f6d84d8f9ed76b9df971
mat <giomatfois62@yahoo.it>
2022-11-09 00:37:42 +0100

    added new entry option

--- rofi-autostart.sh
+++ rofi-autostart.sh
@@ -5,102 +5,133 @@ ROFI_CMD="rofi -dmenu -i -markup-rows"
 
 # TODO: display a "new entry" field to create .desktop files
 list_entries() {
-	# handle empty XDG_CURRENT_DESKTOP env var
-	current_desktop="$XDG_CURRENT_DESKTOP"
-	if [ ${#current_desktop} -eq 0 ]; then
-		current_desktop="asdasdasd"
-	fi
+    # handle empty XDG_CURRENT_DESKTOP env var
+    current_desktop="$XDG_CURRENT_DESKTOP"
+    if [ ${#current_desktop} -eq 0 ]; then
+	    current_desktop="asdasdasd"
+    fi
 
-	all_files=$(find "$AUTOSTART_DIR" -type f -iname "*.desktop")
-	only_show=$(grep "OnlyShowIn" "$AUTOSTART_DIR"/*.desktop | grep -v "$current_desktop" | cut -f1 -d":")
-	not_show=$(grep -E -H -l "NotShowIn.*$current_desktop" "$AUTOSTART_DIR"/*.desktop)
+    all_files=$(find "$AUTOSTART_DIR" -type f -iname "*.desktop")
+    only_show=$(grep "OnlyShowIn" "$AUTOSTART_DIR"/*.desktop | grep -v "$current_desktop" | cut -f1 -d":")
+    not_show=$(grep -E -H -l "NotShowIn.*$current_desktop" "$AUTOSTART_DIR"/*.desktop)
 
-	# filter out entries that should not be shown in current environment
-	common_files=$(comm -13 <(echo "$not_show" | sort) <(echo "$all_files" | sort))
-	# filter out entries that should be shown only in other environments
-	common_files=$(comm -13 <(echo "$only_show" | sort) <(echo "$common_files" | sort))
+    # filter out entries that should not be shown in current environment
+    common_files=$(comm -13 <(echo "$not_show" | sort) <(echo "$all_files" | sort))
+    # filter out entries that should be shown only in other environments
+    common_files=$(comm -13 <(echo "$only_show" | sort) <(echo "$common_files" | sort))
 
-	echo "$common_files"
+    echo "$common_files"
 }
 
 print_entry() {
-	if [ $(grep -i "hidden=true" "$1") ]; then
-		echo $(basename "$1" .desktop) " " "Disabled"
-	else
-		echo $(basename "$1" .desktop) " " "<b>Enabled</b>"
-	fi
+    if [ $(grep -i "hidden=true" "$1") ]; then
+	echo $(basename "$1" .desktop) " " "Disabled"
+    else
+	echo $(basename "$1" .desktop) " " "<b>Enabled</b>"
+    fi
 }
 
-gen_menu() {
-	is_enabled=$(echo "$@" | grep "Enabled")
-	if [ ${#is_enabled} -gt 0 ]; then
-		echo "Disable"
-	else
-		echo "Enable"
-	fi
+gen_entry_menu() {
+    is_enabled=$(echo "$@" | grep "Enabled")
+    if [ ${#is_enabled} -gt 0 ]; then
+	    echo "Disable"
+    else
+	    echo "Enable"
+    fi
 
-	filename=$(echo "$1" | cut -f1 -d" ")
-	desktop_file="$AUTOSTART_DIR/$filename".desktop
-	cmd=$(grep "Exec=" $desktop_file | cut -f2 -d"=" | head -n 1)
+    filename=$(echo "$1" | cut -f1 -d" ")
+    desktop_file="$AUTOSTART_DIR/$filename".desktop
+    cmd=$(grep "Exec=" $desktop_file | cut -f2 -d"=" | head -n 1)
 
-	if [ $(pgrep -f "$cmd") ]; then
-		echo "Stop"
-	else
-		echo "Start"
-	fi
+    if [ $(pgrep -f "$cmd") ]; then
+	    echo "Stop"
+    else
+	    echo "Start"
+    fi
 
-	echo "Edit"
+    echo "Edit"
 }
 
 declare -A actions=(
-	["Enable"]=enable_app
-	["Disable"]=disable_app
-	["Start"]=start_app
-	["Stop"]=stop_app
-	["Edit"]=edit_app
+    ["Enable"]=enable_app
+    ["Disable"]=disable_app
+    ["Start"]=start_app
+    ["Stop"]=stop_app
+    ["Edit"]=edit_app
 )
 
 enable_app() {
-	original_file="$AUTOSTART_DIR/$1".desktop
-	dst_file="$HOME/.config/autostart/$1".desktop
+    original_file="$AUTOSTART_DIR/$1".desktop
+    dst_file="$HOME/.config/autostart/$1".desktop
 
-	# remove line with "Hidden=true"
-	sed -i "/Hidden=/d" "$original_file"
-	cp "$original_file" "$dst_file"
+    # remove line with "Hidden=true"
+    sed -i "/Hidden=/d" "$original_file"
+    cp "$original_file" "$dst_file"
 }
 
 disable_app() {
-	original_file="$AUTOSTART_DIR/$1".desktop
-	dst_file="$HOME/.config/autostart/$1".desktop
+    original_file="$AUTOSTART_DIR/$1".desktop
+    dst_file="$HOME/.config/autostart/$1".desktop
 
-	# append line with "Hidden=true"
-	echo "Hidden=true" >> "$original_file"
-	cp "$original_file" "$dst_file"
+    # append line with "Hidden=true"
+    echo "Hidden=true" >> "$original_file"
+    cp "$original_file" "$dst_file"
 }
 
 start_app() {
-	desktop_file="$AUTOSTART_DIR/$1".desktop
-	cmd=$(grep "Exec=" $desktop_file | cut -f2 -d"=" | head -n 1)
+    desktop_file="$AUTOSTART_DIR/$1".desktop
+    cmd=$(grep "Exec=" $desktop_file | cut -f2 -d"=" | head -n 1)
 
-	$cmd &
+    $cmd &
 }
 
 stop_app() {
-	desktop_file="$AUTOSTART_DIR/$1".desktop
-	cmd=$(grep "Exec=" $desktop_file | cut -f2 -d"=" | head -n 1)
+    desktop_file="$AUTOSTART_DIR/$1".desktop
+    cmd=$(grep "Exec=" $desktop_file | cut -f2 -d"=" | head -n 1)
 
-	pkill -f $cmd
+    pkill -f $cmd
 }
 
 edit_app() {
-	original_file="$AUTOSTART_DIR/$1".desktop
-	dst_file="$HOME/.config/autostart/$1".desktop
+    original_file="$AUTOSTART_DIR/$1".desktop
+    dst_file="$HOME/.config/autostart/$1".desktop
 
-	cp "$original_file" "$dst_file"
-	xdg-open $dst_file
+    cp "$original_file" "$dst_file"
+    xdg-open $dst_file
 
+    rm -rf "$AUTOSTART_DIR"
+    exit 0
+}
+
+desktop-entry() {
+cat <<EOF
+[Desktop Entry]
+Name=$1
+Exec=$1
+Terminal=false
+Type=Application
+EOF
+}
+
+add_entry() {
+    entry_name=$((echo) | rofi -dmenu -p "Entry Name")
+
+    if [ ${#entry_name} -gt 0 ]; then
+	dst_file="$HOME/.config/autostart/$entry_name".desktop
+
+	if [ ! -f "$dst_file" ]; then
+	    desktop-entry "$entry_name" > "$dst_file"
+	fi
+
+	xdg-open "$dst_file"
 	rm -rf "$AUTOSTART_DIR"
 	exit 0
+    fi
+}
+
+gen_menu() {
+    echo "+New Entry"
+    echo "$(list_entries | xargs -I {} bash -c "print_entry {}" | column -t)"
 }
 
 # sort by Enabled(Disabled) state
@@ -114,16 +145,20 @@ export -f print_entry
 # remember last selected entry
 selected_row=0
 
-while selected=$(list_entries | xargs -I {} bash -c "print_entry {}" | column -t | $ROFI_CMD -p "Autostart" -selected-row ${selected_row} -format 'i s'); do
-	selected_row=$(echo "$selected" | awk '{print $1;}')
+while selected=$(gen_menu | $ROFI_CMD -p "Autostart" -selected-row ${selected_row} -format 'i s'); do
+    selected_row=$(echo "$selected" | awk '{print $1;}')
     selected_text=$(echo "$selected" | cut -d' ' -f2-)
-	selected_entry=$(echo $selected_text | cut -f1 -d" ")
+    selected_entry=$(echo $selected_text | cut -f1 -d" ")
 
-	action=$(gen_menu "$selected_text" | $ROFI_CMD -p "$selected_entry")
-	
+    if [ "$selected_text" == "+New Entry" ]; then
+	add_entry
+    else
+	action=$(gen_entry_menu "$selected_text" | $ROFI_CMD -p "$selected_entry")
+
 	if [ ${#action} -gt 0 ]; then
-		${actions[$action]} "$selected_entry";
+	    ${actions[$action]} "$selected_entry";
 	fi
+    fi
 done
 
 rm -rf "$AUTOSTART_DIR"
--- rofi-passmenu.sh
+++ rofi-passmenu.sh
@@ -8,17 +8,17 @@ ROFI_CMD="rofi -dmenu -i -p Password"
 
 typeit=0
 if [[ $1 == "--type" ]]; then
-	typeit=1
-	shift
+    typeit=1
+    shift
 fi
 
 if [[ -n $WAYLAND_DISPLAY ]]; then
-	xdotool="ydotool type --file -"
+    xdotool="ydotool type --file -"
 elif [[ -n $DISPLAY ]]; then
-	xdotool="xdotool type --clearmodifiers --file -"
+    xdotool="xdotool type --clearmodifiers --file -"
 else
-	echo "Error: No Wayland or X11 display detected" >&2
-	exit 1
+    echo "Error: No Wayland or X11 display detected" >&2
+    exit 1
 fi
 
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
@@ -31,7 +31,7 @@ password=$(printf '%s\n' "${password_files[@]}" | $ROFI_CMD "$@")
 [[ -n $password ]] || exit 1
 
 if [[ $typeit -eq 0 ]]; then
-	pass show -c "$password" 2>/dev/null
+    pass show -c "$password" 2>/dev/null
 else
-	pass show "$password" | { IFS= read -r pass; printf %s "$pass"; } | $xdotool
+    pass show "$password" | { IFS= read -r pass; printf %s "$pass"; } | $xdotool
 fi
