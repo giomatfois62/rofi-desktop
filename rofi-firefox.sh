8af6684ed2a9e925f6e5e72029e411773185055e
mat <mat@thinkpad.home>
2023-08-21 23:28:22 +0200

    fixed default profile detection and sqlite3 query

--- rofi-firefox.sh
+++ rofi-firefox.sh
@@ -8,11 +8,10 @@
 
 ROFI_CMD="${ROFI_CMD:-rofi -dmenu -i}"
 
-# TODO: fix this query to return all bookmarks
-SQL="SELECT b.title || \" | \" ||  p.url  FROM moz_bookmarks b JOIN moz_places p ON b.fk = p.id WHERE b.fk is not null AND b.title <> '' AND url <> '' AND url NOT LIKE 'place:%'"
+SQL="SELECT b.title, p.url  FROM moz_bookmarks b JOIN moz_places p ON b.fk = p.id WHERE b.fk is not null AND b.title <> '' AND url <> '' AND url NOT LIKE 'place:%'"
 
-# https://askubuntu.com/questions/239543/get-the-default-firefox-profile-directory-from-bash
-FIREFOX_PROFILE=$(grep "Default=.*\.default-release*" "$HOME/.mozilla/firefox/profiles.ini" | cut -d"=" -f2)
+# https://github.com/ant-arctica/rofi-bookmarks/blob/main/rofi-bookmarks.py
+FIREFOX_PROFILE=$(grep "Default=" "$HOME/.mozilla/firefox/installs.ini" | cut -d"=" -f2)
 
 if [ -z "${FIREFOX_PROFILE+x}" ]; then
 	echo "FIREFOX_PROFILE not set"
@@ -24,7 +23,7 @@ TMP_PLACES=$HOME/.cache/firefox-places
 
 #avoiding db lock
 cp -f ${PROFILE_DB} ${TMP_PLACES}
-ENTRIES=$(sqlite3 ${TMP_PLACES} "${SQL}" | $ROFI_CMD -p "Firefox")
+ENTRIES=$(sqlite3 -separator " | " ${TMP_PLACES} "${SQL}" | $ROFI_CMD -p "Firefox")
 
 IFS=' | ' 
 read -ra ADDR <<< "$ENTRIES"
