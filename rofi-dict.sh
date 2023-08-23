4225f8c453e262a618e0da04a088a5fd2d4494e0
mat <mat@thinkpad.home>
2023-08-23 09:32:18 +0200

    fixed urbandictionary scraping

--- rofi-dict.sh
+++ rofi-dict.sh
@@ -12,16 +12,15 @@ pag() {                                                 \
     sed -e 1d                                           \
         -e 's; _\([A-Z]\); \1;p'                        \
         -e '/^$/d' -e '/^-->/d'                         \
-    | eval "$ROFI_CMD" -l 20 -p 'Done'
+    | eval "$ROFI_CMD" -p 'Done'
 }
 
-# TODO: fix urbandictionary scraping
 while phrase="$(echo $src | eval "$ROFI_CMD" -markup -p 'Lookup: ')"; do
     {
         sdcv -n --utf8-input --utf8-output "$phrase"
         printf "Urban\n"
         links -dump "https://www.urbandictionary.com/define.php?term=$phrase" \
-            | sed -n '1,/Top def/d;/Get the $phrase mug/q;p'
+            | sed -e '1,75d'| head -n -24 | sed '/^   Flag/d' | sed '/^   Get the /d'
     } | pag
 done
 
