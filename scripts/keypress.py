8bafa82149e951bd24502b58280667fd1df03e32
mat <mat@thinkpad.home>
2023-09-20 23:34:49 +0200

    handle alt key to show hud menu

--- scripts/keypress.py
+++ scripts/keypress.py
@@ -6,29 +6,47 @@ import subprocess
 from pynput.keyboard import Key, Listener
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-command = script_dir+"/rofi-desktop.sh -a"
 
-pressed = False
+cmd_command = script_dir+"/rofi-desktop.sh -a"
+alt_command = script_dir+"/hud.py"
+
+cmd_pressed = False
+alt_pressed = False
 
 def on_press(key):
-	global pressed
-	if key == Key.cmd:
-		pressed = True
-	else:
-		pressed = False
-		
-def on_release(key):
-	global pressed
+	global cmd_pressed
+	global alt_pressed
 
-	if key == Key.cmd and pressed:
-		pressed = False
+	cmd_pressed = key == Key.cmd
+	alt_pressed = key == Key.alt
 
-		for process in psutil.process_iter():
+def rofi_running():
+	for process in psutil.process_iter():
 			if process.name() == 'rofi':
 				#process.kill()
-				return
+				return True
 
-		os.system("{} & disown".format(command))
+	return False
+		
+def on_release(key):
+	global cmd_pressed
+	global alt_pressed
+
+	if key == Key.cmd and cmd_pressed:
+		cmd_pressed = False
+
+		if (rofi_running()):
+			return
+
+		os.system("{} & disown".format(cmd_command))
+
+	if key == Key.alt and alt_pressed:
+		alt_pressed = False
+
+		if (rofi_running()):
+			return
+
+		os.system("{} & disown".format(alt_command))
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
 	listener.join()
