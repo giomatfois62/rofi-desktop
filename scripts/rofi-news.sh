f35348bc9a589d6294bd5b0a16c6427cfec762d7
giomatfois62 <matteo.foiss@gmail.com>
2023-09-22 11:11:55 +0200

    load providers and urls from news file

--- scripts/rofi-news.sh
+++ scripts/rofi-news.sh
@@ -5,20 +5,14 @@
 #
 # dependencies: rofi, curl
 
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
+
 ROFI_CMD="${ROFI_CMD:-rofi -dmenu -i}"
-RSS_URL="${RSS_URL:-http://feeds.bbci.co.uk/news/rss.xml?edition=int}"
+RSS_FILE=${RSS_FILE:-"$SCRIPT_PATH/../data/news"}
 RSS_CACHE="${RSS_CACHE:-$HOME/.cache/news}"
 RSS_EXPIRATION_TIME=${RSS_EXPIRATION_TIME:-600} # refresh news file every ten minutes
 
-declare -A rss_urls=(
-	["BBC World"]="http://feeds.bbci.co.uk/news/rss.xml?edition=int"
-	["AP News"]="https://rsshub.app/apnews/topics/apf-topnews"
-	["ANSA.it"]="https://www.ansa.it/sito/ansait_rss.xml"
-	["Al Jazeera"]="https://www.aljazeera.com/xml/rss/all.xml"
-	["BuzzFeed"]="https://www.buzzfeed.com/index.xml"
-)
-
-providers="BBC World\nAP News\nAl Jazeera\nANSA.it"
+providers=$(cat "$RSS_FILE" | cut -d'=' -f1)
 
 mkdir -p "$RSS_CACHE"
 
@@ -27,44 +21,43 @@ provider_row=0
 
 while provider=$(echo -en "$providers" | $ROFI_CMD -selected-row ${provider_row} -format 'i s' -p "News"); do
 	provider_row=$(echo "$provider" | awk '{print $1;}')
-	provider_text=$(echo "$provider" | cut -d' ' -f2-)
+	provider_name=$(echo "$provider" | cut -d' ' -f2-)
 
-	RSS_URL=${rss_urls[$provider_text]}
-	RSS_FILE="$RSS_CACHE/$provider_text.news"
+	rss_url=$(grep "$provider_name=" "$RSS_FILE" | cut -d'=' -f2-)
+	rss_cache_file="$RSS_CACHE/$provider_name.news"
 
 	# TODO: do this job in background and display message
-	if [ -f "$RSS_FILE" ]; then
+	if [ -f "$rss_cache_file" ]; then
 		# compute time delta between current date and news file date
-		news_date=$(date -r "$RSS_FILE" +%s)
+		news_date=$(date -r "$rss_cache_file" +%s)
 		current_date=$(date +%s)
 
 		delta=$((current_date - news_date))
 
 		# refresh news file if it's too old
 		if [ $delta -gt $RSS_EXPIRATION_TIME ]; then
-			curl --silent "$RSS_URL" -o "$RSS_FILE"
+			curl --silent "$rss_url" -o "$rss_cache_file"
 		fi
 	else
-		curl --silent "$RSS_URL" -o "$RSS_FILE"
+		curl --silent "$rss_url" -o "$rss_cache_file"
 	fi
 
-	selected=$(grep -E '(title>|/title>)' "$RSS_FILE" |\
+	selected=$(grep -E '(title>|/title>)' "$rss_cache_file" |\
 		tail -n +4 | sed -e 's/^[ \t]*//' |\
 		sed -e 's/<title>//' -e 's/<\/title>//' -e 's/<description>/  /'\
 			-e 's/<\/description>//' -e 's/\!\[CDATA\[//' -e 's/\]\]//' |\
 		tr -d '<>,' |\
 		awk '$1=$1' |\
-		$ROFI_CMD -p "$provider_text")
+		$ROFI_CMD -p "$provider_name")
 
 	# get selected news and open corresponding link in browser
 	if [ -n "$selected" ]; then
-		link=$(awk "/$selected/{getline;getline; print}" "$RSS_FILE")
+		link=$(awk "/$selected/{getline;getline; print}" "$rss_cache_file")
 
 		echo "$link" | sed -e 's/<link>//' -e 's/<\/link>//' | xargs -I {} xdg-open {}
 
 		exit 0;
 	fi
-
 done
 
 exit 1
