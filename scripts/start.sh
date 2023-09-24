5f635f2b014d298845a67b71e9613b76e2053f03
mat <mat@thinkpad.home>
2023-09-24 14:04:47 +0200

    set display layout only if there's more than one screen connected; set display layout before setting wallpaper

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,18 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
 
+# set monitor layout
+MONITORS_CACHE=${MONITORS_CACHE:-"$HOME/.cache/monitor-layout"}
+
+if [ -f "$MONITORS_CACHE" ]; then
+    connected_screens=$(xrandr | awk '( $2 == "connected" ){ print $1 }' | wc -l)
+
+    if [ "$connected_screens" -gt 1 ]; then
+        echo "Setting display layout"
+        $(cat "$MONITORS_CACHE")
+    fi
+fi
+
 # set wallpaper
 WALLPAPER_CACHE=${WALLPAPER_CACHE:-"$HOME/.cache/wallpaper"}
 
@@ -19,14 +31,6 @@ KEYMAP_CACHE=${KEYMAP_CACHE:-"$HOME/.cache/keymap"}
 if [ -f "$KEYMAP_CACHE" ]; then
     echo "Setting keymap" "$(cat "$KEYMAP_CACHE")"
     setxkbmap "$(cat "$KEYMAP_CACHE")"
-fi
-
-# set monitor layout
-MONITORS_CACHE=${MONITORS_CACHE:-"$HOME/.cache/monitor-layout"}
-
-if [ -f "$MONITORS_CACHE" ]; then
-    echo "Setting display layout"
-    $(cat "$MONITORS_CACHE")
 fi
 
 # TODO: export env vars
