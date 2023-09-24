84a42387e344445554b0ec39f8da7ecf1bb2a4c6
mat <mat@thinkpad.home>
2023-09-24 14:00:29 +0200

    added rofi-desktop startup script

---
+++ scripts/start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# rofi-desktop startup script
+#
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
+
+# set wallpaper
+WALLPAPER_CACHE=${WALLPAPER_CACHE:-"$HOME/.cache/wallpaper"}
+
+if [ -f "$WALLPAPER_CACHE" ]; then
+    echo "Setting wallpaper"
+    "$SCRIPT_PATH/set-wallpaper.sh" "$WALLPAPER_CACHE"
+fi
+
+# set keyboard layout
+KEYMAP_CACHE=${KEYMAP_CACHE:-"$HOME/.cache/keymap"}
+
+if [ -f "$KEYMAP_CACHE" ]; then
+    echo "Setting keymap" "$(cat "$KEYMAP_CACHE")"
+    setxkbmap "$(cat "$KEYMAP_CACHE")"
+fi
+
+# set monitor layout
+MONITORS_CACHE=${MONITORS_CACHE:-"$HOME/.cache/monitor-layout"}
+
+if [ -f "$MONITORS_CACHE" ]; then
+    echo "Setting display layout"
+    $(cat "$MONITORS_CACHE")
+fi
+
+# TODO: export env vars
+
+run_program() {
+    is_running=$(ps aux | grep "$1" | wc -l)
+
+    if [ ${is_running} -lt 2 ]; then
+        echo "running" "$1"
+        "$1" & disown
+    else
+        echo "$1" "already running"
+    fi
+}
+
+run_program "$SCRIPT_PATH/appmenu-service.py"
+run_program "$SCRIPT_PATH/keypress.py"
+
+# TODO: run greenclip
+# TODO: run rofication-daemon
