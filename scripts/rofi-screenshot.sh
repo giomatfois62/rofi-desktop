037b9b16f60958a74f8115c8a271cfe33daa5980
Matteo <matteo.fois@profilocolore.it>
2023-09-29 13:55:28 +0200

    reworked screenshot menu to add full wayland compatibilty

--- scripts/rofi-screenshot.sh
+++ scripts/rofi-screenshot.sh
@@ -2,14 +2,13 @@
 #
 # this script allows taking screenshots of the desktop
 #
-# dependencies: rofi
-# optional: scrot, flameshot, spectacle, xfce4-screenshooter
+# dependencies: rofi, scrot/grim, slurp/flameshot/spectacle/xfce4-screenshooter
 
 ROFI_CMD="${ROFI_CMD:-rofi -dmenu -i}"
 SCREENSHOT_NAME="${SCREENSHOT_NAME:-Screenshot_%Y-%m-%d-%S-%H%M%S.png}"
 
-# check for available programs
-declare -a programs=("flameshot" "spectacle" "xfce4-screenshooter")
+# check for available programs working in both wayland and x11
+declare -a programs=("flameshot" "spectacle")
 
 # launch program if found on system
 for cmd in "${programs[@]}"; do
@@ -23,26 +22,68 @@ for cmd in "${programs[@]}"; do
     fi
 done
 
-# fallback on scrot
-if ! command -v scrot &> /dev/null
-then
-    rofi -e "Install a screenshot program."
+if [ -n "$WAYLAND_DISPLAY" ]; then
+    # fallback on grim
+    if ! command -v grim &> /dev/null
+    then
+        rofi -e "Install grim or a screenshot program for wayland."
+        exit 1
+    fi
+
+    screen_cmd() {
+        sleep 1 ; grim "$(xdg-user-dir PICTURES)/$SCREENSHOT_NAME" ; xdg-open "$(xdg-user-dir PICTURES)/$SCREENSHOT_NAME"
+    }
+
+    area_cmd() {
+        grim -g "$(slurp)" "$(xdg-user-dir PICTURES)/$SCREENSHOT_NAME" ; xdg-open "$(xdg-user-dir PICTURES)/$SCREENSHOT_NAME"
+    }
+
+    window_cmd() {
+        grim -g "$(slurp)" "$(xdg-user-dir PICTURES)/$SCREENSHOT_NAME" ; xdg-open "$(xdg-user-dir PICTURES)/$SCREENSHOT_NAME"
+    }
+elif [ -n "$DISPLAY" ]; then
+    # try xfce4-screenshoter
+    if command -v "xfce4-screenshooter" &> /dev/null; then
+        xfce4-screenshooter
+        exit 0
+    fi
+
+    # fallback on scrot
+    if ! command -v scrot &> /dev/null
+    then
+        rofi -e "Install scrot or a screenshot program for X11."
+        exit 1
+    fi
+
+    screen_cmd() {
+        sleep 1 ; scrot "$SCREENSHOT_NAME" -e 'mv $f $$(xdg-user-dir PICTURES) ; xdg-open $$(xdg-user-dir PICTURES)/$f'
+    }
+
+    area_cmd() {
+        scrot -s "$SCREENSHOT_NAME" -e 'mv $f $$(xdg-user-dir PICTURES) ; xdg-open $$(xdg-user-dir PICTURES)/$f'
+    }
+
+    window_cmd() {
+        scrot -s "$SCREENSHOT_NAME" -e 'mv $f $$(xdg-user-dir PICTURES) ; xdg-open $$(xdg-user-dir PICTURES)/$f'
+    }
+else
+    echo "Error: No Wayland or X11 display detected" >&2
     exit 1
 fi
 
-options="Screen\nDArea\nWindow"
+options="Screen\nArea\nWindow"
 
 chosen="$(echo -e $options | $ROFI_CMD -p 'Screenshot')"
 
 case $chosen in
     "Screen")
-        sleep 1; scrot $SCREENSHOT_NAME -e 'mv $f $$(xdg-user-dir PICTURES) ; xdg-open $$(xdg-user-dir PICTURES)/$f'
+        screen_cmd
         exit 0;;
     "Area")
-        scrot -s $SCREENSHOT_NAME -e 'mv $f $$(xdg-user-dir PICTURES) ; xdg-open $$(xdg-user-dir PICTURES)/$f'
+        area_cmd
         exit 0;;
     "Window")
-        scrot -s $SCREENSHOT_NAME -e 'mv $f $$(xdg-user-dir PICTURES) ; xdg-open $$(xdg-user-dir PICTURES)/$f'
+        window_cmd
         exit 0;;
 esac
 
