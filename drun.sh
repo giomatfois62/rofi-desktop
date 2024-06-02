77267c73d61dd3faeddb9eab97ded6b2d5e35af4
Matteo <giomatfois62@yahoo.it>
2024-06-02 17:21:56 +0200

    improved drun script, accept DRUN_CATEGORIES param

--- drun.sh
+++ drun.sh
@@ -4,4 +4,14 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
 
-PATH="$SCRIPT_PATH/scripts/:$PATH" XDG_DATA_DIRS="$SCRIPT_PATH:/usr/local/share:/usr/share:$XDG_DATA_DIRS" rofi -show drun -show-icons
+DRUN_CATEGORIES=${DRUN_CATEGORIES:-}
+
+if [ -n "$DRUN_CATEGORIES" ]; then
+    categories="-drun-categories $DRUN_CATEGORIES"
+fi
+
+export ROFI_CMD="rofi -dmenu -i -kb-screenshot Control+Shift+space"
+export PATH="$SCRIPT_PATH/scripts/:$PATH"
+export XDG_DATA_DIRS="$SCRIPT_PATH:/usr/local/share:/usr/share:$XDG_DATA_DIRS"
+
+rofi -show drun -show-icons $categories
