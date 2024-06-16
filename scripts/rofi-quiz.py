6f3e7b381e70794b0ae9194bd43436c1b933313e
Matteo <giomatfois62@yahoo.it>
2024-06-16 22:58:29 +0200

    improved quiz

--- scripts/rofi-quiz.py
+++ scripts/rofi-quiz.py
@@ -7,6 +7,22 @@ import random
 from subprocess import Popen,PIPE
 
 url="https://opentdb.com/api.php?amount=1"
+points = 0
+
+def call_rofi(choices, mesg):
+    p1 = Popen(["echo", choices], stdout=PIPE)
+    p2 = Popen(["rofi", "-dmenu", "-mesg", mesg, "-i", "-p", "Answer"], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    
+    answer = p2.communicate()[0].decode('utf8').rstrip()
+    
+    if answer.endswith("\n"):
+        answer = answer[:-1]
+    
+    p1.terminate()
+    p2.terminate()
+    
+    return answer
 
 while (True):
     r = requests.get(url)
@@ -15,35 +31,45 @@ while (True):
     if (len(data["results"]) <= 0):
         break
     
+    start = time.time()
+    
     category = data["results"][0]["category"]
     question = data["results"][0]["question"]
+    difficulty = data["results"][0]["difficulty"]
     correct_ans = data["results"][0]["correct_answer"]
     answers =  data["results"][0]["incorrect_answers"]
     answers.append(correct_ans)
-    mesg = "<b>"+category+"</b>" + "\n\n" + question
-    
     random.shuffle(answers)
+    mesg = f"<b>{category}</b>\n\n{question}"
     choices = "\n".join(answers)
     
-    p1 = Popen(["echo", choices], stdout=PIPE)
-    p2 = Popen(["rofi", "-dmenu", "-mesg", mesg, "-i", "-p", "Answer"], stdin=p1.stdout, stdout=PIPE)
-    p1.stdout.close()
-    answer = p2.communicate()[0].decode('utf8').rstrip()
-    
-    if answer.endswith("\n"):
-        answer = answer[:-1]
+    answer = call_rofi(choices, mesg)
     
     if (len(answer) == 0):
         break
-
-    p2.terminate()
     
     if (answer == correct_ans):
-        p2 = Popen(["rofi", "-e", "Correct!"])
+        if (difficulty == "hard"):
+            win = 5
+        elif (difficulty == "medium"):
+            win = 3
+        else:
+            win = 1
+        
+        points += win
+        mesg += f"\n\n<b>{answer}</b>\nCorrect! +{win} Points ({points} total)"
     else:
-        p2 = Popen(["rofi", "-e", "Wrong!"])
+        mesg += f"\n\n<b>{answer}</b>\nWrong! Correct answer: <b>{correct_ans}</b>"
     
-    p2.wait()
+    choices = "Play Again\nExit"
     
-    #with open(f"data{i}.json", 'w') as f:
-    #    json.dump(data, f)
+    answer = call_rofi(choices, mesg)
+    
+    if (len(answer) == 0 or answer == "Exit"):
+        break
+    
+    end = time.time()
+    
+    # give time for the api to recharge
+    if (end - start < 6):
+        time.sleep(int(6 - (end - start)))
