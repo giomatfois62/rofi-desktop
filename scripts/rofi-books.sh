fb3ea3f4507f88411ec541e0954d8fecd23c8c74
Matteo <giomatfois62@yahoo.it>
2025-01-19 22:31:03 +0100

    improved theme switching

--- scripts/rofi-books.sh
+++ scripts/rofi-books.sh
@@ -8,22 +8,30 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
 
 ROFI="${ROFI:-rofi}"
 ROFI_CACHE_DIR="${ROFI_CACHE_DIR:-$HOME/.cache}"
-BOOKS_PLACEHOLDER="Type something and press \"Enter\" to search books"
-BOOKS_CACHE="$ROFI_CACHE_DIR/books"
 BOOK_ICONS="${BOOK_ICONS:-}"
-PREVIEW_CMD="$SCRIPT_PATH/download_icon.sh {input} {output} {size}"
-SHOW_PREVIEW=false
-SHORTCUTS="-kb-custom-1 "Alt+p""
-MESG="<b>Alt+P</b> toggle preview"
+GRID_ROWS=${GRID_ROWS:-4}
+GRID_COLS=${GRID_COLS:-5}
+GRID_ICON_SIZE=${GRID_ICON_SIZE:-4}
+LIST_ICON_SIZE=${LIST_ICON_SIZE:-3}
+
+book_cache="$ROFI_CACHE_DIR/books"
+preview_cmd="$SCRIPT_PATH/download_icon.sh {input} {output} {size}"
+rofi_shortcuts="-kb-custom-1 Alt+q -kb-custom-2 Alt+w -kb-custom-3 Alt+e"
+rofi_mesg="<b>Enter</b> download book | <b>Alt+Q</b> list-view | <b>Alt+W</b> icons-view | <b>Alt+E</b> list+preview"
+rofi_flags="-eh 2 -sep | -markup-rows"
+rofi_theme_list="element-icon{size:$LIST_ICON_SIZE.0em;}element-text{vertical-align:0.5;}listview{lines:7;}"
+rofi_theme_grid="element{orientation:vertical;}element-text{horizontal-align:0.5;}element-icon{size:$GRID_ICON_SIZE.0em;}listview{lines:$GRID_ROWS;columns:$GRID_COLS;}"
+rofi_theme_preview="mainbox{children:[wrap,listview-split];}wrap{expand:false;orientation:vertical;children:[inputbar,message];}icon-current-entry{expand:true;size:30%;}element-icon{size:3em;}element-text{vertical-align:0.5;}listview-split{orientation:horizontal;children:[listview,icon-current-entry];}listview{lines:7;}"
+rofi_theme="$rofi_theme_list"
 
 mkdir -p "$ROFI_CACHE_DIR"
 
 if [ -n "$BOOK_ICONS" ]; then
-    flags="-show-icons"
+    rofi_flags="$rofi_flags -show-icons"
 fi
 
 if [ -z $1 ]; then
-    query=$(echo "" | $ROFI -dmenu -i -theme-str "entry{placeholder:\"$BOOKS_PLACEHOLDER\";"} -p "Search Books")
+    query=$(echo "" | $ROFI -dmenu -i -p "Search Books")
 else
     query=$1
 fi
@@ -32,59 +40,56 @@ if [ -z "$query" ]; then
     exit 1
 fi
 
-toggle_preview() {
-    if [ "$SHOW_PREVIEW" = false ]; then
-        SHOW_PREVIEW=true
-        theme_str="mainbox{children:[wrap,listview-split];}wrap{expand:false;orientation:vertical;children:[inputbar,message];}icon-current-entry{expand:true;size:40%;}element-icon{size:3em;}element-text{vertical-align:0.5;}listview-split{orientation:horizontal;children:[listview,icon-current-entry];}listview{lines:8;}"
-    else
-        SHOW_PREVIEW=false
-        theme_str="element-icon{size:3em;}element-text{vertical-align:0.5;}listview{lines:8;}"
-    fi
-}
-
-toggle_preview
-toggle_preview
-
 while [ -n "$query" ]; do
     # search first results page
     counter=1
     selected_row=1
 
-    "$SCRIPT_PATH/scrape_books.py" "$query" "$counter" > "$BOOKS_CACHE"
-    result_count=$(cat "$BOOKS_CACHE" | wc -l)
+    "$SCRIPT_PATH/scrape_books.py" "$query" "$counter" > "$book_cache"
+    result_count=$(cat "$book_cache" | wc -l)
 
     if [ "$result_count" -lt 1 ]; then
         $ROFI -e "No results found, try again."
         exit 1
     fi
 
-    books=$(cat "$BOOKS_CACHE" | cut -d' ' -f2- | sed -z 's/\n/|/g' | sed 's/||/\n/g')
+    books=$(cat "$book_cache" | cut -d' ' -f2- | sed -z 's/\n/|/g' | sed 's/||/\n/g')
     books="$books""More..."
 
     # display menu
     while true; do
-        selection=$(echo -en "$books" | $ROFI -dmenu -i -p "Book" -format 'i s' -sep "|" -eh 2 -selected-row ${selected_row} $SHORTCUTS -theme-str "$theme_str" $flags -mesg "$MESG" -preview-cmd "$PREVIEW_CMD")
+        selection=$(echo -en "$books" | \
+            $ROFI -dmenu -i \
+            $rofi_shortcuts \
+            $rofi_flags \
+            -p "Book" \
+            -format 'i s' \
+            -selected-row ${selected_row} \
+            -theme-str "$rofi_theme" \
+            -mesg "$rofi_mesg" \
+            -preview-cmd "$preview_cmd")
         exit_code="$?"
         
         row=$(($(echo "$selection" | cut -d' ' -f1) + 1))
         book=$(echo "$selection" | cut -d' ' -f2-)
 
-        if [ "$exit_code" -eq 10 ]; then
-            toggle_preview
-        elif [ -z "$book" ]; then
-            break
-        elif [ "$book" = "More..." ]; then
+        [ "$exit_code" -eq 1 ] && break
+        [ "$exit_code" -eq 10 ] && rofi_theme="$rofi_theme_list"
+        [ "$exit_code" -eq 11 ] && rofi_theme="$rofi_theme_grid"
+        [ "$exit_code" -eq 12 ] && rofi_theme="$rofi_theme_preview"
+        
+        if [ "$book" = "More..." ]; then
             # increment page counter and search again
             counter=$((counter+1))
             selected_row=$row
 
-            "$SCRIPT_PATH/scrape_books.py" "$query" "$counter" >> "$BOOKS_CACHE"
+            "$SCRIPT_PATH/scrape_books.py" "$query" "$counter" >> "$book_cache"
 
-            books=$(cat "$BOOKS_CACHE" | cut -d' ' -f2- | sed -z 's/\n/|/g' | sed 's/||/\n/g')
+            books=$(cat "$book_cache" | cut -d' ' -f2- | sed -z 's/\n/|/g' | sed 's/||/\n/g')
             books="$books""More..."
-        else
+        elif [ "$exit_code" -eq 0 ]; then
             # open selected url
-            url=$(sed "${row}q;d" "$BOOKS_CACHE" | cut -d' ' -f1)
+            url=$(sed "${row}q;d" "$book_cache" | cut -d' ' -f1)
 
             xdg-open "$url"
 
@@ -92,7 +97,7 @@ while [ -n "$query" ]; do
         fi
     done
 
-    query=$(echo "" | $ROFI -dmenu -i -theme-str "entry{placeholder:\"$BOOKS_PLACEHOLDER\";"} -p "Search Books")
+    query=$(echo "" | $ROFI -dmenu -i -p "Search Books")
 done
 
 exit 1
