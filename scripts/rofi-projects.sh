c1798df1a5d662b957404b1d7a799222445bc71b
Matteo <giomatfois62@yahoo.it>
2025-01-24 00:28:38 +0100

    renamed variable

--- scripts/rofi-projects.sh
+++ scripts/rofi-projects.sh
@@ -6,17 +6,17 @@
 
 ROFI="${ROFI:-rofi}"
 PROJECTS_EDITOR="${PROJECTS_EDITOR:-"$HOME"/Programs/qtcreator/bin/qtcreator}"
-PROJECTS_DIRECTORY="${PROJECTS_DIRECTORY:-"$HOME"/Projects}"
+PROJECTS_DIR="${PROJECTS_DIR:-"$HOME"/Projects}"
 
 if ! command -v $PROJECTS_EDITOR &> /dev/null; then
     $ROFI -e "Projects Editor $PROJECTS_EDITOR not found"
     exit 1
 fi
 
-choice=$(ls "$PROJECTS_DIRECTORY" | $ROFI -dmenu -i -p "Project")
+choice=$(ls "$PROJECTS_DIR" | $ROFI -dmenu -i -p "Project")
 
 if [ -n "$choice" ]; then
-    $PROJECTS_EDITOR "$PROJECTS_DIRECTORY/$choice" & disown
+    $PROJECTS_EDITOR "$PROJECTS_DIR/$choice" & disown
     exit 0
 fi
 
