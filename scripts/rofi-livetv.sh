b173d6e31927bf88258da2227deb47f5332658b3
Matteo <giomatfois62@yahoo.it>
2025-06-13 21:27:44 +0200

    remove seconds from event date; download event country icons

--- scripts/rofi-livetv.sh
+++ scripts/rofi-livetv.sh
@@ -4,12 +4,15 @@
 #
 # dependencies: rofi, jq, curl
 
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
+
 ROFI="${ROFI:-rofi}"
 ROFI_CACHE_DIR="${ROFI_CACHE_DIR:-$HOME/.cache}"
 
 livetv_url="https://webmaster.livetv.club/list.php?id=21&sport=&sp=&r=_ru"
 livetv_refresh=3600 # refresh livetv file every hour
 livetv_file="$ROFI_CACHE_DIR/livetv"
+livetv_preview="$SCRIPT_PATH/download_icon.sh {input} {output} {size}"
 
 fix_link() {
     local link="$1"
@@ -39,7 +42,10 @@ fi
 matches=$(cat "$livetv_file" | grep -o -P '(?<=ev_arr = ).*(?=;)')
 channels=$(cat "$livetv_file" | grep -o -P '(?<=chan_arr = ).*(?=;)')
 
-while match=$(echo "$matches" | jq -r '.[] | "\(.date) [\(.sport)]\(.match)"' | $ROFI -dmenu -i -p "Events"); do
+while match=$(echo "$matches" |\
+    jq -r '.[] | "\(.date | sub(":00"; "")) [\(.sport)]\(.match)<ICON>\(.country)"' |\
+    sed -e "s/<ICON>/\\x00icon\\x1fthumbnail:\/\/https:\/\/livetv.club\/img\/countries\//g" |\
+    $ROFI -show-icons -dmenu -i -p "Events" -preview-cmd "$livetv_preview"); do
     match_name=$(echo "$match" | cut -d']' -f2-)
     match_id=$(echo "$matches" | jq ".[] | select(.match==\"$match_name\") | .id")
     match_links=$(echo "$channels" | jq -r ".[$match_id]" | jq -r ".[] | select(.type==\"Flash\") | .link")
